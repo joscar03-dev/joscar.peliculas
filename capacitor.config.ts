cb1aff451be0209472560cf6a51bcdc1a3b8ac9b
joscar03-dev <136681347+joscar03-dev@users.noreply.github.com>
2024-11-03 18:07:43 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'joscar.peliculas',
+  webDir: 'www'
+};
+
+export default config;
