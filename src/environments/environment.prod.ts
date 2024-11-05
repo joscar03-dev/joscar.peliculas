1867cab28e797f09c8cec4182e32cbbd4cd16f69
joscar03-dev <136681347+joscar03-dev@users.noreply.github.com>
2024-11-04 21:01:04 -0500

    varibales en enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  url: 'https://api.themoviedb.org/3',
+  apiKey: '0deb41837541d4cc04e6872f5ff3fafc',
+  imgPath: 'https://image.tmdb.org/t/p',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  url: 'https://api.themoviedb.org/3',
+  apiKey: '0deb41837541d4cc04e6872f5ff3fafc',
+  imgPath: 'https://image.tmdb.org/t/p',
 };
 
 /*
