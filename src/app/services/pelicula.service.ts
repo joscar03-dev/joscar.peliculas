8fd0a4f79f1be998fa27b5032042fb0474c5692c
joscar03-dev <136681347+joscar03-dev@users.noreply.github.com>
2024-11-06 07:56:03 -0500

    agregado de servicios: cartelera, populares, detalle y creditos

--- src/app/services/pelicula.service.ts
+++ src/app/services/pelicula.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { IRespuestaTMDB } from '../models/pelicula.model';
+import { IDetallePelicula, IRespCredits, IResponseCarteleraTMDB, IRespuestaTMDB } from '../models/pelicula.model';
 import { environment } from 'src/environments/environment';
 
 const URL = environment.url;
@@ -10,12 +10,13 @@ const apiKey = environment.apiKey;
 })
 export class PeliculaService {
   private http = inject(HttpClient);
+  private popularPage = 0;
 
   constructor() {}
 
   private ejecutarQuery<T>(query: string) {
-    const url = `${URL}/${query}&api_key=${apiKey}`;
-    console.log('Request URL:', url);
+    const url = `${URL}/${query}&api_key=${apiKey}&language=es-PE`;
+    // console.log('Request URL:', url);
     return this.http.get<T>(url);
   }
 
@@ -35,5 +36,22 @@ export class PeliculaService {
     return this.ejecutarQuery<IRespuestaTMDB>(
       `discover/movie?primary_release_date.gte=${fInicio}&primary_release_date.lte=${fFin}&primary_release_date.`
     );
+  }
+
+  getCartelera(){
+    return this.ejecutarQuery<IResponseCarteleraTMDB>(`movie/now_playing?a=1`);
+  }
+
+  getPopulares(){
+    this.popularPage++;
+    return this.ejecutarQuery<IRespuestaTMDB>(`movie/popular?page=${this.popularPage}`);
+  }
+
+  getDetallePelicula(id: number){
+    return this.ejecutarQuery<IDetallePelicula>(`movie/${id}?a=1`);
+  }
+
+  getCreditosPeli(id : number){
+    return this.ejecutarQuery<IRespCredits>(`movie/${id}/credits?a=1`);
   }
 }
