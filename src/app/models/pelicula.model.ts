6915140cab24f6774e445faed8ce46dfc7816920
joscar03-dev <136681347+joscar03-dev@users.noreply.github.com>
2024-11-06 07:56:39 -0500

    nuevas interfaces para cartelera, populares, creditos, etc

--- src/app/models/pelicula.model.ts
+++ src/app/models/pelicula.model.ts
@@ -21,3 +21,105 @@ export interface IPelicula {
   vote_average:      number;
   vote_count:        number;
 }
+
+export interface IResponseCarteleraTMDB {
+  dates:         Dates;
+  page:          number;
+  results:       IPelicula[];
+  total_pages:   number;
+  total_results: number;
+}
+
+export interface Dates {
+  maximum: string;
+  minimum: string;
+}
+
+
+export interface IDetallePelicula {
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection: null;
+  budget: number;
+  genres: Genre[];
+  homepage: string;
+  id: number;
+  imdb_id: string;
+  origin_country: string[];
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: Productioncompany[];
+  production_countries: Productioncountry[];
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: Spokenlanguage[];
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+export interface Spokenlanguage {
+  english_name: string;
+  iso_639_1: string;
+  name: string;
+}
+
+export interface Productioncountry {
+  iso_3166_1: string;
+  name: string;
+}
+
+export interface Productioncompany {
+  id: number;
+  logo_path: string;
+  name: string;
+  origin_country: string;
+}
+
+export interface Genre {
+  id: number;
+  name: string;
+}
+
+
+export interface IRespCredits {
+  id: number;
+  cast: Cast[];
+  crew: Crew[];
+}
+
+export interface Crew {
+  adult: boolean;
+  gender: number;
+  id: number;
+  known_for_department: string;
+  name: string;
+  original_name: string;
+  popularity: number;
+  profile_path: null | string;
+  credit_id: string;
+  department: string;
+  job: string;
+}
+
+export interface Cast {
+  adult: boolean;
+  gender: number;
+  id: number;
+  known_for_department: string;
+  name: string;
+  original_name: string;
+  popularity: number;
+  profile_path: null | string;
+  cast_id: number;
+  character: string;
+  credit_id: string;
+  order: number;
+}
